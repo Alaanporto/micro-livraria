7b63f17c101aef8b381d147c93b77f06bec07a5f
Alaanporto <alaanporto@gmail.com>
2021-08-22 15:34:37 -0300

    Tarefa prática #1 - Microservices

--- services/controller/index.js
+++ services/controller/index.js
@@ -41,6 +41,17 @@ app.get('/shipping/:cep', (req, res, next) => {
         }
     );
 });
+app.get('/product/:id', (req, res, next) => {
+    // Chama método do microsserviço.
+    inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
+        // Se ocorrer algum erro de comunicação
+        // com o microsserviço, retorna para o navegador.
+        if (err) {
+            console.error(err);
+            res.status(500).send({ error: 'something failed :(' });
+        } else {
+            // Caso contrário, retorna resultado do
+            // microsserviço (um arquivo JSON) 
 
 /**
  * Inicia o router
